0735f91b3ab69841c1fcac6f8a187718816f5817
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-03-26 12:17:27 +0530

    Add memoryManagment.js file for managing memory operation of calculator

---
+++ memoryManagment.js
@@ -0,0 +1,68 @@
+// memory operations 
+import {
+    setInputString,
+    getInputString,
+    addInputStr,
+    setDisplayStr,
+    addDisplayStr,
+    updateDisplay,
+    getDisplayStr
+} from "./index.js"
+
+let memory=+localStorage.getItem('memoryValue')||0;
+
+ 
+export function updatememorybutton(){
+    
+    let hasMemory=memory===0?false:true;
+    document.querySelector('#col-mc-style').disabled=!hasMemory;
+    document.querySelector('#col-mr-style').disabled=!hasMemory;
+}
+
+// memory addition M+
+export function memoryPlus() {
+    let inputStr = getInputString();
+    memory+=eval(inputStr);
+    localStorage.setItem('memoryValue',memory);
+    updatememorybutton();
+    console.log(memory);
+}
+
+// memory subtraction 
+export function memorySubtraction() {
+    let inputStr = getInputString();
+    memory -= eval(inputStr);
+    localStorage.setItem('memoryValue', memory);
+    updatememorybutton();
+    console.log(memory);
+}
+
+// memory Read
+
+export function memoryRead() {
+   
+    let displayStr = getDisplayStr();
+    displayStr = memory;
+    console.log(memory);
+    
+     setDisplayStr(displayStr);
+    // addDisplayStr(memory)
+    updateDisplay()
+    
+}
+
+//memory clear
+export function memoryClear() {
+    memory=0;
+    localStorage.removeItem('memoryValue');
+    updatememorybutton();
+}
+ 
+// save display value to memory
+export function memorySave() {
+    let inputStr = getInputString();
+    memory=eval(inputStr);
+    localStorage.setItem('memoryValue',memory);
+    updatememorybutton();
+    console.log(memory);
+}
